daf0f94267fc1e2f43457fc94ca64e526f4a0155
Igor Alentev <alentev.olm@ya.ru>
2020-10-03 20:22:36 +0300

    ghjgh

---
+++ LD47/Assets/Scripts/GlobalController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalController : MonoBehaviour
+{
+    private Character char1;
+    private Character char2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        char1 = this.gameObject.AddComponent<Character>();
+        char2 = this.gameObject.AddComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- LD47/Assets/Scripts/InputManager.cs
+++ LD47/Assets/Scripts/InputManager.cs
@@ -13,6 +13,37 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {    
+            print("Works W");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {    
+            
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {    
+            
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {    
+            
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {    
+            
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {    
+            
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {    
+            
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {    
+            print("Works ->");
+        }
     }
 }
