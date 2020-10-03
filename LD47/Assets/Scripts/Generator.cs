97f8d22e9fa3af1a66bf098570b1ced09a533f22
Max3kkk <fedorovm633@gmail.com>
2020-10-03 21:46:55 +0300

    Generator

--- LD47/Assets/Scripts/Generator.cs
+++ LD47/Assets/Scripts/Generator.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Generator : MonoBehaviour
 {
     [SerializeField] private float cellSize = 1f;
+    public Generator Gen;
 
     [SerializeField] private GameObject CellPrefab;
     public int rows, columns;
+    public Sprite StartCellSprite;
     public Character[] Characters;
     public Cell[,] CellMatrix;
 
@@ -18,20 +20,29 @@ public class Generator : MonoBehaviour
         //just for starting el-s
         for (int j = 0; j < columns; j++)
         {
-            Vector3 spawnPoint = new Vector3(0, j*cellSize,0); 
+            Vector3 spawnPoint = new Vector3(j*cellSize, 0,0); 
             CellMatrix[0, j] = Instantiate(CellPrefab, spawnPoint, Quaternion.identity).GetComponent<Cell>();
             CellMatrix[0, j].MyState = Cell.State.StartingPoint;
             
-            spawnPoint = new Vector3(cellSize * (rows + 1), j*cellSize,0); 
+            spawnPoint = new Vector3(j*cellSize, -cellSize * (rows + 1),0); 
             CellMatrix[rows + 1, j] = Instantiate(CellPrefab, spawnPoint, Quaternion.identity).GetComponent<Cell>();
             CellMatrix[rows + 1, j].MyState = Cell.State.StartingPoint;
+            
+            CellMatrix[rows + 1, j].GetComponent<SpriteRenderer>().sprite = StartCellSprite;
+            CellMatrix[0, j].GetComponent<SpriteRenderer>().sprite = StartCellSprite;
         }
 
         for (int i = 1; i < rows + 1; i++)
         {
             for (int j = 0; j < columns; j++)
-                i = 0;
+            {
+                Vector3 spawnPoint = new Vector3(j *cellSize, -i*cellSize,0); 
+                CellMatrix[rows + 1, j] = Instantiate(CellPrefab, spawnPoint, Quaternion.identity).GetComponent<Cell>();
+                CellMatrix[rows + 1, j].MyState = Cell.State.Surface;
+            }
         }
 
     }
+    
+    
 }
