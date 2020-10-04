c27fe9381337ab35662ed0b8b0661518e026415a
Genom10 <genom_10@mail.ru>
2020-10-05 00:13:54 +0300

    Scene changing preparation

---
+++ LD47/Assets/Scripts/Starter.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+    public GameObject generator;
+
+    void Start()
+    {
+        generator.GetComponent<Generator>().GenerateMatrix(10, 20);
+        generator.GetComponent<Generator>().SpawnChar();
+    }
+
+}
